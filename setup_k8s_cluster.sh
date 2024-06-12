019634ca2e72ce4a3ff79893206cd2318274f483
Talha Waheed <talhawaheed57@gmail.com>
2024-06-12 17:33:03 -0500

    hotelReservation now working

--- setup_k8s_cluster.sh
+++ setup_k8s_cluster.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
-# set -x
+set -x
 set -e
 
 NODES=3
 
-# # delete any previous minikube cluster
+# echo "[SCRIPT] Deleting any previous minikube cluster..."
 # minikube delete --all
 
-# # use minikube to start an empty k8s cluster
+# echo "[SCRIPT] Starting empty minikube cluster with $NODES nodes [2 CPU / 4096 MB] using VirtualBox..."
 # minikube start --nodes $((NODES+1)) --cpus 2 --memory 4096 --driver=virtualbox
 
-# sleep 10
+# SLEEP_TIME=10
+# echo "[SCRIPT] Sleeping for $SLEEP_TIME seconds..."
+# sleep $SLEEP_TIME
 
-# # set labels
+# echo "[SCRIPT] Setting labels on each node..."
 # for i in $(seq 1 $NODES);
 # do
 #   kubectl label node minikube-m0$(($i+1)) node-role.kubernetes.io/worker=node$i --overwrite
 # done
 
-# spawn host agents on each node
+# echo "[SCRIPT] Installing istio..."
+# istioctl install -y
+# kubectl label namespace default istio-injection=enabled
+# # kubectl rollout restart deploy
+
+echo "[SCRIPT] Starting HotelReservation..."
+kubectl apply -Rf hotelReservation/kubernetes
+
+# echo "[SCRIPT] Applying istio configs for hotelReservation..."
+# kubectl apply -f istio-configs/hotelReservation.yaml
+
+echo "[SCRIPT] Spawning host agents on each node..."
 kubectl apply -f host_agent/pod_svc_for_master_node.yaml
 for i in $(seq 1 $NODES)
 do
