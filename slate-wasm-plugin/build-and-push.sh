10a5462d625d51c7f4758351064f46f99bc171cb
Talha Waheed <talhawaheed57@gmail.com>
2024-06-14 14:35:44 -0500

    virtual service for frontend works now (through the istio ingress gateway)

---
+++ slate-wasm-plugin/build-and-push.sh
@@ -0,0 +1,11 @@
+set -e
+
+# for cloudlab
+GOARCH=wasm GOOS=js /usr/local/bin/tinygo build -o wasm-out/slate_plugin.wasm -gc=custom -tags="custommalloc nottinygc_envoy" -scheduler=none -target=wasi main.go
+
+# for aditya: tinygo location is different
+#  GOARCH=wasm GOOS=js $HOME/go/bin/tinygo build -o wasm-out/slate_plugin.wasm -gc=custom -tags="custommalloc nottinygc_envoy" -scheduler=none -target=wasi main.go
+
+
+docker build -t ghcr.io/adiprerepa/slate-plugin:latest .
+docker push ghcr.io/adiprerepa/slate-plugin:latest
